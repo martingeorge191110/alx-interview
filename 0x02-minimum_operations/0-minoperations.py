81e34b0f9706a41f5fb0f140d9f94acd625f7d09
Martin George <martingeorge191110@gmail.com>
2025-05-02 00:23:22 +0300

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""write a method that calculates
+the fewest number of operations
+needed to result in exactly n H
+characters in the file."""
+
+
+def minOperations(n):
+    return 0
